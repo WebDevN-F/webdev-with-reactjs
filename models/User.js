614d50648a882b18e57eb761e11fa0d1ac9df7de
ngnam <nguyenvannam0411@gmail.com>
2022-01-16 23:23:52 +0700

    commit

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
 }, { timestamps: true }, { strict: false });
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,52 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET).toString()
     });
 
     try {
-        await newUser.save();
+        const saveduser = await newUser.save();
         res.status(201).json({
             message: 'User created successfully',
-            user: newUser
+            user: saveduser
         });
     } catch (err) {
-        console.warn(err);
+        res.status(500).json({
+            message: err
+        });
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+            return res.status(401).json({
+                message: 'User not found'
+            });
+        }
+
+        const decrypted = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET).toString(CryptoJS.enc.Utf8);
+        if (decrypted !== password) {
+            return res.status(401).json({
+                message: 'Password is incorrect'
+            });
+        }
+
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        res.header('auth-token', token).json({
+            token: token,
+            user: user.username
+        });
+    } catch (err) {
         res.status(500).json({
             message: err
         });
