643f9c2babe260e5ae2c03e33ee13a025ef6d9a6
ngnam <nguyenvannam0411@gmail.com>
2022-01-18 01:04:46 +0700

    commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -11,6 +12,15 @@ mongoose
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+app.use(cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    exposedHeaders: 'web-auth-token', 
+    allowedHeaders: 'web-auth-token, Content-Type', 
+    maxAge: '3600',
+    optionsSuccessStatus: 200,
+}));
 app.use(bodyParser.json());
 
 app.use('/api/users', require('./routes/user'));
@@ -18,6 +28,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/product'));
 app.use('/api/cart', require('./routes/cart'));
 app.use('/api/order', require('./routes/order'));
+app.use('/api/checkout', require('./routes/stripe'));
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Ecommecer API Server running on port', process.env.PORT);
---
+++ routes/stripe.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+router.post('/', (req, res) => {
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: 'usd'
+    }, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({ error: stripeErr });
+        } else {
+            res.status(200).send({ success: stripeRes });
+        }
+    });
+})
+
+module.exports = router;
