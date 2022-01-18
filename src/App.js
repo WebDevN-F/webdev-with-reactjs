772b77e66c7b5e65ae6bf87aad50b34cda40baa5
ngnam <nguyenvannam0411@gmail.com>
2022-01-19 03:34:23 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={
+        <Route path="/" index element={
           <React.Suspense fallback={<>loading...</>}>
             <HomePage />
           </React.Suspense>}
