93193bcd784552c9f6481110f2abfdfcb4353e9a
ngnam <nguyenvannam0411@gmail.com>
2022-01-19 19:13:39 +0700

    commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression')
+const app = express();
+const publicPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 5000;
+app.use(express.static(publicPath));
+app.use(compression())
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+    console.log('Server is up!', port);
+});
