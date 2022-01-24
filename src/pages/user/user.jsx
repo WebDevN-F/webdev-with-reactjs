bc7b6520cca2f1f82392c626917c049e0192fec2
ngnam <nguyenvannam0411@gmail.com>
2022-01-25 00:36:15 +0700

    cm

--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,41 +1,112 @@
 import React from 'react';
 import './user.css';
-import { DataGrid } from '@mui/x-data-grid';
+import { IconButton } from '@mui/material';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {
+    DataGrid,
+    GridOverlay,
+    gridPageCountSelector,
+    gridPageSelector,
+    useGridApiContext,
+    useGridSelector,
+} from '@mui/x-data-grid';
+import Pagination from '@mui/material/Pagination';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
+const CustomPagination = () => {
+    const apiRef = useGridApiContext();
+    const page = useGridSelector(apiRef, gridPageSelector);
+    const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+
+    return (
+        <Pagination
+            color="primary"
+            count={pageCount}
+            page={page + 1}
+            onChange={(event, value) => apiRef.current.setPage(value - 1)}
+        />
+    );
+}
+
+function CustomLoadingOverlay() {
+    return (
+      <GridOverlay>
+        <div style={{ position: 'absolute', top: 0, width: '100%' }}>
+          <LinearProgress />
+        </div>
+      </GridOverlay>
+    );
+  }
 
 const User = () => {
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'username', headerName: 'User Name', width: 200 },
+        {
+            field: 'user', headerName: 'User', width: 200, renderCell: (params) => {
+                return (
+                    <div className="user-listname">
+                        <img src={params.row.avatar} alt={params.row.user} className="avatar" />
+                        <span className="user-name">{params.row.user}</span>
+                    </div>
+                )
+            }
+        },
         { field: 'email', headerName: 'Email', width: 250 },
         {
-            field: 'orders',
-            headerName: 'Total Orders',
+            field: 'transactions',
+            headerName: 'Transaction volumn',
             type: 'number',
-            width: 120,
+            width: 220,
+            renderCell: (params) => {
+                return (
+                    <span className="transaction-volumn">$ {params.row.transactions}</span>
+                )
+            }
         },
+        {
+            field: 'action',
+            headerName: 'Action',
+            width: 100,
+            renderCell: (params) => {
+                return (
+                    <div className="user-action">
+                        <IconButton className="user-action-edit">
+                            <ModeEditIcon />
+                        </IconButton>
+                        <IconButton className="user-action-delete">
+                            <DeleteIcon />
+                        </IconButton>
+                    </div>
+                )
+            }
+        }
     ];
-    
-    const rows = [
-        { id: 1, username: 'Snow', email: 'Jon', orders: 35 },
-        { id: 2, username: 'Lannister', email: 'Cersei', orders: 42 },
-        { id: 3, username: 'Lannister', email: 'Jaime', orders: 45 },
-        { id: 4, username: 'Stark', email: 'Arya', orders: 16 },
-        { id: 5, username: 'Targaryen', email: 'Daenerys', orders: null },
-        { id: 6, username: 'Melisandre', email: null, orders: 150 },
-        { id: 7, username: 'Clifford', email: 'Ferrara', orders: 44 },
-        { id: 8, username: 'Frances', email: 'Rossini', orders: 36 },
-        { id: 9, username: 'Roxie', email: 'Harvey', orders: 65 },
-    ];
+
+    const rows = Array.from(Array(1000).keys()).map(i => ({
+        id: i,
+        user: `User ${i}`,
+        email: `webdevA${i}@gmail.com`,
+        transactions: Math.floor(Math.random() * 100000),
+        avatar: '/50x50.png',
+    }));
 
     return (
         <div className="userContainer">
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[10]}
-                checkboxSelection
-            />
+            <Box sx={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={100}
+                    rowsPerPageOptions={[100, 200, 300, 400, 500]}
+                    checkboxSelection
+                    components={{
+                        Pagination: CustomPagination,
+                        LoadingOverlay: CustomLoadingOverlay,
+                    }}
+                />
+            </Box>
         </div>
     )
 };
