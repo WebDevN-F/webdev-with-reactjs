77fa8e77ce3994d4c14092cadb391fc2c86ace01
ngnam <nguyenvannam0411@gmail.com>
2022-04-02 02:54:48 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Nav from './components/Nav';
+import Dashboard from './pages/dashboard';
+import TicketPage from './pages/ticketPage';
+
 function App() {
   return (
-    <>  
-      <h1>Hello World</h1>
-    </>
+    <div className="app">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/ticket" element={<TicketPage />} />
+          <Route path="/ticket/:id" element={<TicketPage editMode={true} />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
